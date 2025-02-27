298f2aa3fd676b0566ba35967b69796ebbc0f6c5
MarcoA366 <marco_95_5@yahoo.com>
2025-02-27 11:06:34 -0300

    md

--- src/vistas/Viajes/TablaViajes.jsx
+++ src/vistas/Viajes/TablaViajes.jsx
@@ -1,48 +1,42 @@
 // import React from "react";
 // import { useData } from "../../context/DataContext";
 // import { Card, Row, Col } from "react-bootstrap";
-
+// import "./TablaViajes.css"
 // const ListaViajes = () => {
-//   const { viajes } = useData();
-//   const viajesOrdenados = [...viajes].sort(
+//   const { viajesCompletados } = useData();
+//   const viajesOrdenados = [...viajesCompletados].sort(
 //     (a, b) => new Date(b.fecha_completado) - new Date(a.fecha_completado)
 //   );
 
 //   return (
-//     <div className="lista_viajes-wrapper m-3">
+//     <div className="lista_viajes m-3">
 //       {viajesOrdenados.map((viaje, index) => (
 //         <Card
 //           key={index}
-//           className="mb-2 p-2 shadow-sm bg-dark text-light border border-secondary"
+//           className="mb-2 p-2 py-0 shadow-sm bg-dark text-light border border-secondary"
 //         >
-//           <Row className="align-items-center">
-//             {/* Detalles del viaje */}
-
-//             <Col xs={2}>
-//               <div className="d-flex flex-column text-center">
-//                 <p className="fw-bold text-secondary">
-//                   {viajesOrdenados.length - index}
-//                 </p>
-//               </div>
+//           <Row className="align-items-center h-100">
+//             <Col xs={2} className="d-flex flex-column text-center justify-content-center">
+//               <p className="fw-bold fs-2 text-secondary">
+//                 {viajesOrdenados.length - index}
+//               </p>
 //             </Col>
-//             <Col xs={8}>
-//               <div className="d-flex justify-content-between text-center w-100">
+
+//             <Col xs={8} className="d-flex flex-column gap-1 p-1 justify-content-center">
+//               <div className="d-flex justify-content-between align-items-center gap-2 text-center p-0 w-100">
 //                 <p className="fw-bold text-warning">{viaje.origen}</p>
 //                 <p className="fw-bold text-success">{viaje.destino || "?"}</p>
 //               </div>
-
-//               <div className="d-flex justify-content-between text-center w-100">
+//               <div className="d-flex justify-content-between align-items-center text-center p-0 w-100">
 //                 <p className="fw-bold text-warning">
 //                   {viaje.fecha_asignado_time}
 //                 </p>
 //                 <p className="fw-bold text-success">{viaje.fecha_completado_time}</p>
 //               </div>
 //             </Col>
-
-//             <Col xs={2}>
-//               <div className="d-flex flex-column text-center">
-//                 <p className="fw-bold">${viaje.importe}</p>
-//               </div>
+            
+//             <Col xs={2} className="d-flex flex-column text-center justify-content-center">
+//               <p className="fw-bold">${viaje.importe}</p>
 //             </Col>
 //           </Row>
 //         </Card>
@@ -56,10 +50,18 @@
 
 
 
+
+
+
+
+
+
+
 import React from "react";
 import { useData } from "../../context/DataContext";
 import { Card, Row, Col } from "react-bootstrap";
 import "./TablaViajes.css"
+import { FaInfoCircle, FaRegFileAlt } from "react-icons/fa";
 const ListaViajes = () => {
   const { viajesCompletados } = useData();
   const viajesOrdenados = [...viajesCompletados].sort(
@@ -80,22 +82,25 @@ const ListaViajes = () => {
               </p>
             </Col>
 
-            <Col xs={8} className="d-flex flex-column gap-1 p-1 justify-content-center">
-              <div className="d-flex justify-content-between align-items-center gap-2 text-center p-0 w-100">
+            <Col xs={3} className="d-flex flex-column gap-1 p-1 justify-content-center align-items-center text-center">
                 <p className="fw-bold text-warning">{viaje.origen}</p>
-                <p className="fw-bold text-success">{viaje.destino || "?"}</p>
-              </div>
-              <div className="d-flex justify-content-between align-items-center text-center p-0 w-100">
+                {/* <p className="fw-bold text-success">{viaje.destino || "?"}</p> */}
+            </Col>
+
+
+            <Col xs={3} className="d-flex flex-column gap-1 p-1 justify-content-center align-items-center">
                 <p className="fw-bold text-warning">
                   {viaje.fecha_asignado_time}
                 </p>
-                <p className="fw-bold text-success">{viaje.fecha_completado_time}</p>
-              </div>
+                {/* <p className="fw-bold text-success">{viaje.fecha_completado_time}</p> */}
             </Col>
             
-            <Col xs={2} className="d-flex flex-column text-center justify-content-center">
+            <Col xs={4} className="d-flex flex-column text-center justify-content-center">
               <p className="fw-bold">${viaje.importe}</p>
             </Col>
+            {/* <Col xs={1} className="d-flex justify-content-center">
+            <FaRegFileAlt className="text-secondary" />
+            </Col> */}
           </Row>
         </Card>
       ))}
